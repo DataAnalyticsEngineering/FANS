523b90d130ae46d27bc29adaa1ee5ef9cdfe03fe
ac133718 <keshav@mib.uni-stuttgart.de>
2024-07-01 11:18:55 +0200

    test

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 #nohup /usr/bin/time -v mpiexec -n 16 ./FANS input_files/3d0_MFL_0_mech.json &
 #nohup taskset -c 0 /usr/bin/time -v mpiexec -n 1 ./FANS input_files/berea128_h5_mech.json &
 # nohup /usr/bin/time -v mpiexec -n 4 ./FANS input_files/sphere32_hyper_elastic.json > nohup.out 2>&1 &
-nohup /usr/bin/time -v mpiexec -n 16 ./FANS input_files/sphere256_linear_elastic.json > nohup1.out 2>&1 &
+nohup /usr/bin/time -v mpiexec -n 16 ./FANS input_files/sphere256_linear_elastic.json > nohup.out 2>&1 &
 #mpiexec -n 1 ./FANS input_files/berea128_h5_thermal.json
