a38f22db978e9fdb826a081f0e8fdf8e06d9d37f
ac133718 <keshav@mib.uni-stuttgart.de>
2024-07-11 15:41:55 +0200

    updated h5 to xdmf viz pipeline

--- test/run.sh
+++ test/run.sh
@@ -1,3 +1,1 @@
-nohup /usr/bin/time -v mpiexec --bind-to core -n 2 ./FANS input_files/test_ThermalLinear.json test_results.h5 > nohup_ThermalLinear.log 2>&1 &
-nohup /usr/bin/time -v mpiexec --bind-to core -n 2 ./FANS input_files/test_MechLinear.json test_results.h5 > nohup_MechLinear.log 2>&1 &
-nohup /usr/bin/time -v mpiexec --bind-to core -n 2 ./FANS input_files/test_HyperElastic.json test_results.h5 > nohup_HyperElastic.log 2>&1 &
+nohup /usr/bin/time -v mpiexec --bind-to core -n 16 ./FANS input_files/sphere_ThermalLinear.json test_results.h5 > nohup.log 2>&1 &
