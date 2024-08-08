4bc0e6e85cbeeeecb68e0203cd71f39be8f9f067
ac133718 <keshav@mib.uni-stuttgart.de>
2024-08-08 13:18:54 +0200

    refactored postprocess in solver.h

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 # Check if the number of processes is provided as a command line argument
@@ -16,8 +15,3 @@ nohup time -v mpiexec -n $num_processes ./FANS input_files/test_LinearElasticIso
 nohup time -v mpiexec -n $num_processes ./FANS input_files/test_PseudoPlasticLinearHardening.json test_PseudoPlasticLinearHardening.h5 > test_PseudoPlasticLinearHardening.log 2>&1 &
 
 nohup time -v mpiexec -n $num_processes ./FANS input_files/test_VonMisesPlasticLinearIsotropicHardening.json test_VonMisesPlasticLinearIsotropicHardening.h5 > test_VonMisesPlasticLinearIsotropicHardening.log 2>&1 &
-=======
-nohup /usr/bin/time -v mpiexec -n 2 ./FANS input_files/test_ThermalLinear.json test_results.h5 > nohup_test_ThermalLinear.log 2>&1 &
-nohup /usr/bin/time -v mpiexec -n 2 ./FANS input_files/test_MechLinear.json test_results.h5 > nohup_test_MechLinear.log 2>&1 &
-nohup /usr/bin/time -v mpiexec -n 2 ./FANS input_files/test_HyperElastic.json test_results.h5 > nohup_test_HyperElastic.log 2>&1 &
->>>>>>> restructured how to define macrscale loading in the input file
