3504a1c339266118e8724b57241b729d78074f00
Sanath <sanathkeshav.mysore@gmail.com>
2025-05-12 16:59:50 +0200

    formatting docstring

--- include/material_models/GBDiffusion.h
+++ include/material_models/GBDiffusion.h
@@ -38,7 +38,7 @@
  *   {
  *     "GB_unformity": false,
  *     "D_bulk": [...],  // Array of length (num_crystals + num_GB elements), but D_bulk is only used for crystals (0 to num_crystals)
- *     "D_par":  [...],   // Array of length (num_crystals + num_GB elements), but D_par  is only used for GBs (num_crystals to num_crystals + num_GB)
+ *     "D_par":  [...],  // Array of length (num_crystals + num_GB elements), but D_par  is only used for GBs (num_crystals to num_crystals + num_GB)
  *     "D_perp": [...]   // Array of length (num_crystals + num_GB elements), but D_perp is only used for GBs (num_crystals to num_crystals + num_GB)
  *   }
  */
