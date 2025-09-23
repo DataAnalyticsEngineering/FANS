7cedd4c28ee1162c1e74b119c833b42033e4fd18
Sanath Keshav <sanathkeshav.mysore@gmail.com>
2025-09-23 14:54:15 +0200

    add macos to pixi build matrix (#84)
    Co-authored-by: Claudius Haag <claudius.haag@mib.uni-stuttgart.de>

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -15,18 +15,15 @@ else
     FANS_EXEC="./FANS"
 fi
 
-TIME_CMD="command time -v"
-[[ "$OSTYPE" == "darwin"* ]] && TIME_CMD="command gtime -v"
-
 mkdir -p output
 
 # Run the jobs serially
-$TIME_CMD mpiexec -n $num_processes "$FANS_EXEC" input_files/test_LinearThermal.json output/test_LinearThermal.h5 > test_LinearThermal.log 2>&1
+command time -v mpiexec -n $num_processes "$FANS_EXEC" input_files/test_LinearThermal.json output/test_LinearThermal.h5 > output/test_LinearThermal.log 2>&1
 
-$TIME_CMD mpiexec -n $num_processes "$FANS_EXEC" input_files/test_LinearElastic.json output/test_LinearElastic.h5 > test_LinearElastic.log 2>&1
+command time -v mpiexec -n $num_processes "$FANS_EXEC" input_files/test_LinearElastic.json output/test_LinearElastic.h5 > output/test_LinearElastic.log 2>&1
 
-$TIME_CMD mpiexec -n $num_processes "$FANS_EXEC" input_files/test_PseudoPlastic.json output/test_PseudoPlastic.h5 > test_PseudoPlastic.log 2>&1
+command time -v mpiexec -n $num_processes "$FANS_EXEC" input_files/test_PseudoPlastic.json output/test_PseudoPlastic.h5 > output/test_PseudoPlastic.log 2>&1
 
-$TIME_CMD mpiexec -n $num_processes "$FANS_EXEC" input_files/test_J2Plasticity.json output/test_J2Plasticity.h5 > test_J2Plasticity.log 2>&1
+command time -v mpiexec -n $num_processes "$FANS_EXEC" input_files/test_J2Plasticity.json output/test_J2Plasticity.h5 > output/test_J2Plasticity.log 2>&1
 
-$TIME_CMD mpiexec -n $num_processes "$FANS_EXEC" input_files/test_MixedBCs.json output/test_MixedBCs.h5 > test_MixedBCs.log 2>&1
+command time -v mpiexec -n $num_processes "$FANS_EXEC" input_files/test_MixedBCs.json output/test_MixedBCs.h5 > output/test_MixedBCs.log 2>&1
